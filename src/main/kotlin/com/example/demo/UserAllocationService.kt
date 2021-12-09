45ad7672f32bc3fac9f619b7e15b121341723ab5
josh-white <joshua.jay.white@gmail.com>
2021-12-09 15:57:36 -0600

    commiting so i can get on peronsal mac

--- src/main/kotlin/com/example/demo/UserAllocationService.kt
+++ src/main/kotlin/com/example/demo/UserAllocationService.kt
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Service
 @Service
 class UserAllocationService {
     fun saveTeam(teamName: String): Team {
-        TODO("Not yet implemented")
+        val team = Team(
+            name = teamName
+        )
+        TeamRepository.save(team)
     }
 
 }
--- src/test/kotlin/com/example/demo/UserAllocationServiceTest.kt
+++ src/test/kotlin/com/example/demo/UserAllocationServiceTest.kt
@@ -1,5 +1,22 @@
 package com.example.demo
 
+import io.mockk.every
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
 
-internal class UserAllocationServiceTest
+internal class UserAllocationServiceTest {
+    private lateinit var mockTeamRepository: TeamRepository
+    @Test
+    fun `saveTeam saves the team to the database` () {
+        mockTeamRepository = mock()
+
+        val team = Team(
+            id = 1,
+            name = "GOAT Team"
+        )
+
+        every { mockTeamRepository.save("GOAT Team") } returns team
+
+    }
+}
